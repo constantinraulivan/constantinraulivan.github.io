1343b5dcb50c9acdc83570b8ae025b6efbb0bd59
Ivan Constantin Raul <constantinraulivan@gmail.com>
2022-07-04 21:00:33 +0300

    first version of CV

---
+++ index.js
@@ -0,0 +1,22 @@
+console.info(2);
+console.debug(2 + 3);
+console.warn(56 * 3 - 10);
+console.error("app started");
+
+var myName = "Raul";
+var age = 19;
+
+console.info("My name is " + myName + ", I'm " + age + " years old.");
+age = 30;
+console.info("My name is " + myName + ", I'm " + age + " years old.");
+age = 31;
+console.info("My name is " + myName + ", I'm " + age + " years old.");
+
+var jobTitle = "<span>Marketing Specialist</span> la Uny";
+var mottoElement = document.getElementById("motto");
+
+console.info(mottoElement);
+console.warn(mottoElement.innerHTML);
+
+mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
+// mottoElement.innerHTML += " & " + jobTitle;
