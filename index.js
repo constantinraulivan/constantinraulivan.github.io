12453c863dbce26801709b230e4db60b2f9e72ac
Ivan Constantin Raul <constantinraulivan@gmail.com>
2022-07-11 21:44:32 +0300

    switch pages

--- index.js
+++ index.js
@@ -12,11 +12,42 @@ console.info("My name is " + myName + ", I'm " + age + " years old.");
 age = 31;
 console.info("My name is " + myName + ", I'm " + age + " years old.");
 
-var jobTitle = "<span>Marketing Specialist</span> la Uny";
-var mottoElement = document.getElementById("motto");
+// var jobTitle = "<span>Marketing Specialist</span> la Uny";
+// var mottoElement = document.getElementById("motto");
 
-console.info(mottoElement);
-console.warn(mottoElement.innerHTML);
+// console.info(mottoElement);
+// console.warn(mottoElement.innerHTML);
 
-mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
+// mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
 // mottoElement.innerHTML += " & " + jobTitle;
+
+function showHomePage() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showProjectPage() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showLanguagePage() {
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+}
+
+showHomePage();
